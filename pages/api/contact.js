3d4266366029fa6307ce2b73d40fea5027a5bfc9
Louis Rolwes <143202114+lRolwes@users.noreply.github.com>
2024-11-29 13:17:44 -0600

    Update contact.js

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,22 +1,23 @@
 export default function (req, res) {
     require('dotenv').config()
-    const PASSWORD = process.env.password
-    const EMAIL = process.env.email
+    const PASSWORD = process.env.PASSWORD
+    const USERNAME = process.env.USERNAME
+    const EMAIL = process.env.TOEMAIL
 
     let nodemailer = require('nodemailer')
 
     const transporter = nodemailer.createTransport({
         port: 465,
-        host: "smtp.zoho.com",
+        host: "smtp.gmail.com",
         auth: {
-          user: 'mailerlittleom@zohomail.com',
+          user: USERNAME,
           pass: PASSWORD,
         },
         secure: true,
       })
 
     const mailData = {
-        from: 'mailerlittleom@zohomail.com',
+        from: USERNAME,
         to: EMAIL,
         subject: `Message From ${req.body.name}`,
         text: "treatment: " + req.body.treatment + "\n" + req.body.message + " | Sent from: " + req.body.email,
